5682ac6c08c44ef312c29311171b8b7dffdca054
fabiansdp <13519140@std.stei.itb.ac.id>
2026-01-14 15:53:09 +0700

    fix: change lr

--- Scripts/train.sh
+++ Scripts/train.sh
@@ -9,7 +9,7 @@ export CUDA_VISIBLE_DEVICES="$GPU"
 python run_model.py -mode train -model_checkpoint google/mt5-base \
 -experiment_name "${DATASET_TYPE}-${TASK}" -task "$TASK" -output_dir ./models \
 -inst_type 1 \
--learning_rate 1e-4 \
+-learning_rate 2e-4 \
 -save_strategy epoch \
 -id_tr_data_path "./Dataset/${DATASET_TYPE}/${TASK}/train.csv" \
 -id_val_data_path "./Dataset/${DATASET_TYPE}/${TASK}/dev.csv" \
